b989f3fd40f803bdf476ae10aad4468b3be3a278
TEOS <proyectoteos@gmail.com>
2018-04-29 02:57:34 -0500

    Code and diagram available
    Program code and assembly schema

---
+++ Display-de-7-segmentos-con-Arduino.ino
@@ -0,0 +1,40 @@
+/*
+  Created by TEOS
+  Domotic with Arduino https://goo.gl/btZpjs
+  YouTube https://goo.gl/k7TUSZ
+  Instagram https://goo.gl/zEIjia
+  Facebook https://goo.gl/eivraR
+  Twitter https://goo.gl/du5Wgn
+  Github https://goo.gl/Xl5IiS
+  Google Plus https://goo.gl/gBnUdh
+  WEB https://goo.gl/jtiYJy
+*/
+
+int displayPinT;
+
+byte  displayNumberT[10][7] = {
+  {1, 1, 1, 1, 1, 1, 0},
+  {0, 1, 1, 0, 0, 0, 0},
+  {1, 1, 0, 1, 1, 0, 1},
+  {1, 1, 1, 1, 0, 0, 1},
+  {0, 1, 1, 0, 0, 1, 1},
+  {1, 0, 1, 1, 0, 1, 1},
+  {1, 0, 1, 1, 1, 1, 1},
+  {1, 1, 1, 0, 0, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1},
+  {1, 1, 1, 0, 0, 1, 1}
+};
+
+void setup() {
+  for (displayPinT = 2; displayPinT < 9; displayPinT++)
+    pinMode(displayPinT, OUTPUT);
+}
+
+void loop() {
+  for ( int numberPositionT = 0 ; numberPositionT < 10 ; numberPositionT++) {
+    for (int pinPositionT = 0 ; pinPositionT < 7 ; pinPositionT++) {
+      digitalWrite(pinPositionT + 2 , displayNumberT[numberPositionT][pinPositionT] ) ;
+    }
+    delay(1000);
+  }
+}
